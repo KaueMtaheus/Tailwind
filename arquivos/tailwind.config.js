6f22e703435923df2102c64c5d0fdc309cd893cf
Kauê Matheus <kauematheus7@hotmail.com>
2023-10-31 18:43:07 -0300

    tailwind css

---
+++ arquivos/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darhMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        brand: '#c542f5',
+      },
+    },
+  },
+  plugins: [],
+}
+
